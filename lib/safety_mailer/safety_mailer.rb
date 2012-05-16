f9d475ce57aecf2361ae86161483cef25000c623
HuyD & TienL <huy.dinh+tien.le@eastagile.com>
2012-05-16 12:24:51 +0700

    Allow redirect suppressed email to a specified email address, and turn off the safety_mailer dynamically via ENV variable.

--- lib/safety_mailer/safety_mailer.rb
+++ lib/safety_mailer/safety_mailer.rb
@@ -1,29 +1,36 @@
 module SafetyMailer
   class Carrier
-    attr_accessor :matchers, :settings
+    attr_accessor :matchers, :settings, :forward_to
+
     def initialize(params = {})
-      self.matchers = params[:allowed_matchers] || []
-      self.settings = params[:delivery_method_settings] || {}
+      self.matchers   = params[:allowed_matchers] || []
+      self.forward_to = params[:forward_to] || []
+      self.settings   = params[:delivery_method_settings] || {}
       delivery_method = params[:delivery_method] || :smtp
       @delivery_method = Mail::Configuration.instance.lookup_delivery_method(delivery_method).new(settings)
     end
+
     def log(msg)
       Rails.logger.warn(msg) if defined?(Rails)
     end
+
     def deliver!(mail)
-      mail.to = mail.to.reject do |recipient|
-        if matchers.any?{ |m| recipient =~ m }
-          false
-        else
-          log "*** safety_mailer suppressing mail to #{recipient}"
-          true
-        end
-      end
-      if mail.to.nil? || mail.to.empty?
-        log "*** safety_mailer - no recipients left ... suppressing delivery altogether"
-      else
-        log "*** safety_mailer allowing delivery to #{mail.to}"
+      if ENV['DISABLE_SAFETY_MAILER'] == 'true'
         @delivery_method.deliver!(mail)
+      else
+        allowed_emails = mail.to.select do |recipient|
+          matchers.any?{ |m| recipient =~ m }
+        end
+
+        mail.to = allowed_emails
+        mail.to += forward_to if forward_to.any?
+
+        if mail.to.nil? || mail.to.empty?
+          log "*** safety_mailer - no recipients left ... suppressing delivery altogether"
+        else
+          log "*** safety_mailer allowing delivery to #{mail.to}"
+          @delivery_method.deliver!(mail)
+        end
       end
     end
   end
